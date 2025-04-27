5568b331e43241c047cf7a3074875d8c5e7c5b46
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-04-27 20:26:15 +0000

    container  config done

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,1 +1,16 @@
 console.log("Hello Akshat")
+// Data Type-------------
+/*
+let = it is contianer to store a data in a veriable. Currently js use this data type
+var = it is also a continer to store a data type but at that time we not use because variable can not follow a scope
+const = it can be store a variable which can't be change during the program
+*/
+
+const userId = 1001
+let userName = 'Akshat Gupta'
+var userEmail = 'akshatgupta@gmail.com'
+usercity = 'kanpur'   
+let userState ;
+
+console.log(userId)
+console.table([userName, userEmail,usercity, userState]);
