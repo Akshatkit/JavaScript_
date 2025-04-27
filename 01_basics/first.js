46bec8420bf98eddba0d2ac638026de890f55700
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-04-27 21:28:19 +0000

    comtainer comfig done

--- 01_basics/first.js
+++ 01_basics/first.js
@@ -1,11 +1,18 @@
 console.log("Hello Akshat")
-// Data Type-------------
+
+
+
+
+// ----------------------Data Type-------------
+
 /*
 let = it is contianer to store a data in a veriable. Currently js use this data type
-var = it is also a continer to store a data type but at that time we not use because variable can not follow a scope
+var = it is also a continer to store a data type 
+      but at that time we not use because variable can not follow a  block scope and functional Scop
 const = it can be store a variable which can't be change during the program
 */
 
+/*
 const userId = 1001
 let userName = 'Akshat Gupta'
 var userEmail = 'akshatgupta@gmail.com'
@@ -14,3 +21,53 @@ let userState ;
 
 console.log(userId)
 console.table([userName, userEmail,usercity, userState]);
+*/
+
+
+//-----------DATA TYPE---------------
+
+/* premitive -
+number = 
+bigint
+string
+boolian = true/false
+Null = standalone value / object
+undefined = 
+symbol =  use to uniquly identify
+
+
+non -  premitive - 
+
+object
+typeof
+*/
+
+// Type Conversion
+
+// let numType= "akshat"
+// console.log(typeof numType);
+// console.log(typeof(numType));
+
+// let convertVariable = String(numType)
+// console.log(typeof(convertVariable));
+// console.log(convertVariable);
+
+// let convertVariable = Boolean(numType)
+// console.log(typeof(convertVariable));
+// console.log(convertVariable);
+/* in Boolean  
+null, undefine, 0, "" = False
+1, string = true
+
+String -> Number =Opt NaN 
+null , Undefined -> Number = Opt 0
+
+*/
+
+
+
+
+// let StringType;
+// let convertVariable = Number(StringType)
+// console.log(typeof(convertVariable));
+// console.log(convertVariable);
