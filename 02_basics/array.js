202c5f01ce63688c45073ed6e29a4ee99e9e70f3
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-04-29 17:18:04 +0000

    container config done

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,1 +1,33 @@
-console.log("Hello")
+const arr1 = [1,2,3,5,4]
+
+const arr2 = ["akshat","sakshi","lala","kalu"]
+
+// console.log(arr1, arr2)
+// console.log(typeof(arr1))
+
+// arr1.push(10) // add element in last
+// arr1.pop()   //remove element from last
+
+
+// arr1.unshift(12)  // add element at first
+// arr1.shift()      // remove element at first
+// arr1.length      // find len of array
+
+// arr1.sort()       // sort the array
+
+// arr1.concat(arr2)   //combine 2 array
+
+
+
+// console.log(arr1.indexOf(3))
+// console.log(arr1.includes(9))
+
+// slice splice
+
+// console.log("A", arr1)
+// console.log("A", arr1.slice(1,3))
+
+// console.log("B", arr1.splice(1,3))
+
+
+
