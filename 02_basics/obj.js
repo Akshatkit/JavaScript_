7644547cb8f8001c56e20b19a8ff1e878db881bb
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-05-02 11:31:29 +0000

    container config done

--- 02_basics/obj.js
+++ 02_basics/obj.js
@@ -1,14 +1,14 @@
-const newSymbol = Symbol("Key1")
+// const newSymbol = Symbol("Key1")
 
-const obj = {
-    name : "Akshat",
-    cource : "B.Tech",
-    rollNum : "2201650109007" ,
-    branch : "Computer Science And Engineering",
-    Section : "C",
-    PhoneNum : "8574597008",
-    [newSymbol] : "mykey1"   // syntox to acces a symbol data type in object and Type of is undefined 
-}
+// const obj = {
+//     name : "Akshat",
+//     cource : "B.Tech",
+//     rollNum : "2201650109007" ,
+//     branch : "Computer Science And Engineering",
+//     Section : "C",
+//     PhoneNum : "8574597008",
+//     [newSymbol] : "mykey1"   // syntox to acces a symbol data type in object and Type of is undefined 
+// }
 
 // console.log(obj.name)
 // console.log(obj["rollNum"])
@@ -24,16 +24,28 @@ const obj = {
 
 // obj.PhoneNum = "9839435382"
 
-obj.greeting = function(){
-        console.log("Hello, Mr",obj.name);
+// obj.greeting = function(){
+//         console.log("Hello, Mr",obj.name);
     
-}    
+// }    
 
-obj.greetingT = function(){
-    console.log('Hello, Mr ', this.name);  //this function used when same function value call in the function
+// obj.greetingT = function(){
+//     console.log('Hello, Mr ', this.name);  //this function used when same function value call in the function
 
-}    
+// }    
 
-console.log(obj.greeting())
-console.log(obj.greetingT())
+// console.log(obj.greeting())
+// console.log(obj.greetingT())
+
+
+// -------object 2.0 ------------
+
+// const tinderUser = new Object()  // singalton Object
+
+const tinderUser = {
+    email: "akshatBaniyaa@gmail.com",
+    userDetail:{username: "Akshat gupta",
+        userPhnone : {Pnum1:"8858442241",Pnum2:"8574597008"}
+    )
+}
 
