0f2377fde2d0b47d9c59f765be7be22a49eb2080
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-05-02 20:31:00 +0000

    container config done

--- 02_basics/fun.js
+++ 02_basics/fun.js
@@ -1,4 +1,4 @@
-console.log("Hello this is function File")
+console.log("Welcome to function File")
 // function check(){
 //     console.log("This is First Function")
 
@@ -8,58 +8,106 @@ console.log("Hello this is function File")
 
 //----------Function with parameter-----
 
-function add(num1 , num2){
-    let sum = num1+num2
-    console.log(sum)
-}
+// function add(num1 , num2){
+//     let sum = num1+num2
+//     console.log(sum)
+// }
 
 // add(5,5)
 // let sum = add(5,5)
 // console.log(sum)   // undefine
 
-function add(num1 , num2){
+// function add(num1 , num2){
     
-    return num1+num2
-}
+//     return num1+num2
+// }
 // let sum = add(5,5)
 // console.log(sum)
 
 
-function greet(username ){
-    if (!username){
-        console.log("Please Enter user name")
-        return
+// function greet(username ){
+//     if (!username){
+//         console.log("Please Enter user name")
+//         return
 
-    }
-    console.log(`Hello Mr. ${username}`)
+//     }
+//     console.log(`Hello Mr. ${username}`)
 
-}
+// }
 // greet('Akshat')
 
-const user = {
-    name : 'Akshat gupta',
-    price : 200
-}
+// const user = {
+//     name : 'Akshat gupta',
+//     price : 200
+// }
 
-function display(userprint){
-    return `Hi ${userprint.name} your purchasing bill ${userprint.price}`
-}
+// function display(userprint){
+//     return `Hi ${userprint.name} your purchasing bill ${userprint.price}`
+// }
 
 // console.log(display(user))
 
 
 
-function show(...itemPrice){
-    let sum= 0;
-    for (let i =0; i< itemPrice.length;i++){
-        sum = sum + itemPrice[i]
-    }
-    return sum
-}
+// function show(...itemPrice){
+//     let sum= 0;
+//     for (let i =0; i< itemPrice.length;i++){
+//         sum = sum + itemPrice[i]
+//     }
+//     return sum
+// }
 
 // let totalPrice = show(400, 500 , 20,114, 25)
 // console.log(`total Price is ${totalPrice}`)
 
 
 
+// ------------------ Funtion mini hosting--------------
+
+
+// function one(){
+//     const username = "Akshat"
+
+//     function two(){
+//         const chanal = "youtube"
+//         console.log(username )
+//     }
+//     // console.log(chanal)
+//     two()
+// }
+
+// one()
+
+// one()
+
+// function one(){
+//     const username = "Akshat"
+
+//     function two(){
+//         const chanal = "youtube"
+//         console.log(username+ chanal )
+//     }
+//     // console.log(chanal)
+//     two()
+// }
+
+// newFun
+
+// const newFun = new function(){
+//     if(true){
+//         const username = 'Akshat'
+//         if(username === 'Akshat')
+//         {
+//             console.log(`Thanks ${username}`)
+//         }
+//     }
+// }
+
+
+
+// const sumOne = new function(num){  // it is only object
+//     return num + 1
+// }
+
+// console.log(sumOne)
 
