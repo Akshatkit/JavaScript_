778b1a8fce24e459579bbe3d651d40a30f747504
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-05-04 20:11:52 +0000

    container config done

--- 02_basics/ittrator.js
+++ 02_basics/ittrator.js
@@ -8,7 +8,30 @@ console.log("----------------------Ittrator file-----------------------------")
 
 // break And continue
 
+
+
+
+// while (true) {
+//     console.log("1");
+    
+// }   // infinity loop
+
+
+
+
+
+
+// do{
+//   console.log("number")
+// }while(false)
+
+
+
 //-------------------for of loop and for in loop----------------
+
+
+
+//------for off loop -------------
 
 
 //map
@@ -51,14 +74,14 @@ js and the Value is JavaScript
 css and the Value is Cascading Style Sheed  */
 
 // for of in array
-const arr = [10,20,30,40,50,60]
+// const arr = [10,20,30,40,50,60]
 
 // for (const item of arr) {
 //     console.log(item);
 // }
 
-let str = new String()
-str = "Hello World !"
+// let str = new String()
+// str = "Hello World !"
 
 // for (const item of str) {
 //     console.log(item);
@@ -77,3 +100,72 @@ str = "Hello World !"
 //     console.log(item);
     
 // }
+
+
+// for in loop------------------
+
+// const map = new Map()
+// map.set('PY',"PYTHON")
+// map.set('JAVA' , "JAVA")
+// map.set('CPP' , "C++")
+// map.set('rb',"Ruby")
+// map.set('js',"JavaScript")
+// map.set('css',"Cascading Style Sheed")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// } // no output
+
+
+// let obj = { PY: 'PYTHON', 
+//     JAVA: 'JAVA',
+//     CPP: 'C++' ,
+//     rb: 'Ruby',
+//     js: 'JavaScript' ,
+//     css:'Cascading Style Sheed' }
+
+// for (const key in obj) {
+//    console.log(`${obj[key]} there short cut is ${key}`);
+   
+// }
+
+
+
+// for each loop
+
+// let obj = { PY: 'PYTHON', 
+//     JAVA: 'JAVA',
+//     CPP: 'C++' ,
+//     rb: 'Ruby',
+//     js: 'JavaScript' ,
+    // css:'Cascading Style Sheed' }        // not working in obj
+
+// obj.forEach( (key) => {
+//     console.log(key)
+    
+// });
+
+
+const arr =[ 1,2,3,4,5]
+
+// arr.forEach((item) => {
+//     console.log(item);
+    
+    
+// });
+
+
+// arr.forEach(function (item)  {
+//     console.log(item);
+    
+    
+// });
+
+
+
+// function ptr(num) {
+//     console.log(num);
+        
+// }
+// arr.forEach(ptr)
