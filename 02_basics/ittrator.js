51c77a069bdfb34d7bd345f1eba7fdd5f13977bf
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-05-04 19:49:31 +0000

    container config done

--- 02_basics/ittrator.js
+++ 02_basics/ittrator.js
@@ -1,10 +1,79 @@
 console.log("----------------------Ittrator file-----------------------------")
 
-for (let i = 0; i < 10; i++) {
-    const element = i;
-    console.log(element)
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element)
     
-}
+// }
 
 // break And continue
 
+//-------------------for of loop and for in loop----------------
+
+
+//map
+
+// const map = new Map()
+// map.set('PY',"PYTHON")
+// map.set('JAVA' , "JAVA")
+// map.set('CPP' , "C++")
+// map.set('rb',"Ruby")
+// map.set('js',"JavaScript")
+// map.set('css',"Cascading Style Sheed")
+
+// console.log(map)
+
+// for (const item of map) {
+//     console.log(item)
+    
+// }
+/* output ==========
+[ 'PY', 'PYTHON' ]
+[ 'JAVA', 'JAVA' ]
+[ 'CPP', 'C++' ]
+[ 'rb', 'Ruby' ]
+[ 'js', 'JavaScript' ]
+[ 'css', 'Cascading Style Sheed' ]
+ */
+
+
+// for (const [item , value] of map) {
+//     console.log(`${item} and the Value is ${value}`);
+    
+    
+// }
+/* output
+PY and the Value is PYTHON
+JAVA and the Value is JAVA
+CPP and the Value is C++
+rb and the Value is Ruby
+js and the Value is JavaScript
+css and the Value is Cascading Style Sheed  */
+
+// for of in array
+const arr = [10,20,30,40,50,60]
+
+// for (const item of arr) {
+//     console.log(item);
+// }
+
+let str = new String()
+str = "Hello World !"
+
+// for (const item of str) {
+//     console.log(item);
+    
+    
+// }
+
+// let obj = { PY: 'PYTHON', 
+//     JAVA: 'JAVA',
+//     CPP: 'C++' ,
+//     rb: 'Ruby',
+//     js: 'JavaScript' ,
+//     css:'Cascading Style Sheed' }
+
+// for (const [item,value] of obj) {
+//     console.log(item);
+    
+// }
