1ffe5b0c3b83c039cc7df1ae80af33d8b6a1a320
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-05-04 07:38:48 +0000

    container config done

---
+++ 02_basics/ittrator.js
@@ -0,0 +1,10 @@
+console.log("----------------------Ittrator file-----------------------------")
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element)
+    
+}
+
+// break And continue
+
