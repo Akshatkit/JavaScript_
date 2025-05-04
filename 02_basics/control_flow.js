8cb7cc189a737a64be74c3cec76e1b835a8a6be3
Akshat Gupta <134906462+Akshatkit@users.noreply.github.com>
2025-05-04 07:04:14 +0000

    container config done

---
+++ 02_basics/control_flow.js
@@ -0,0 +1,84 @@
+console.log("------------this is control flow file-----------------")
+
+/* if(condotion){    //it lets run when condution is true
+
+    code content
+}
+else if(condotion){    //it lets run when condution is true
+
+    code content
+}
+else if(condotion){    //it lets run when condution is true
+
+    code content
+}
+else{
+
+    code content
+}
+
+
+
+
+*/
+
+// if(true) console.log("Hello");
+
+
+// conparision Operator   => < , > , <= , >= , == , === , != , !== 
+
+
+
+
+// ============ switch statement====================
+
+
+/*
+let key= 2;
+switch(key){
+    case 1:
+        console.log(key)
+        break ;
+    case 2:
+        console.log(key)
+        break ;
+    case 3:
+        console.log(key)
+        break ;
+    case 4:
+        console.log(key)
+        break;
+
+    default:
+        console.log("Default")
+        break
+}
+
+*/
+
+
+
+
+//----------------------------truthy and Falsi value
+
+//truthy =>  "0","False", [], {} , function(){ }
+
+//False => NaN , 0 , -0 , Null , undefined , "Empty string " , bigint 0n 
+
+
+
+// const obj = {}
+
+// if (Object.keys(obj).length==0){
+//     console.log("Empty")
+// }
+
+//===================== Nullish Coalescing Operator ( ?? ) 
+
+// let value ;
+// value = 5 ?? 10;
+// console.log(value)
+
+// icecream = 20
+
+// icecream >= 50 ? console.log("true") : console.log("False");
